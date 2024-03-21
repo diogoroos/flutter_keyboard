86047ceecac35ab7a58216e43f101ada68b0efe5
diogoroos <78812662+diogoroos@users.noreply.github.com>
2024-03-20 21:50:25 -0300

    fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:example/flutter_keyboard.dart';
+import 'package:flutter_keyboard/flutter_keyboard.dart';
 
 void main() => runApp(const MyApp());
 
